2c4cad1a6a14831ad54eccc71ad5bc10e7dc8039
moogen <jwu9995@gmail.com>
2018-09-29 23:24:31 -0500

    Waypoints

--- gmaps_interface.py
+++ gmaps_interface.py
@@ -1,48 +1,39 @@
-import requests 
 import pandas as pd
-# Need python json library 
+import googlemaps
+import sys
 
 API_KEY = "AIzaSyCO6xWAy1F4WEAAArjT8qgEgFjZc77B7ds"
 
-def generate_route(origin, destination): 
+def generate_route(client, origin, destination, waypoint = None): 
 	""" Pings the Google Maps API to generate directions between two locations """
 	# Need to extend this to using waypoints using *args
-	no_comma_orig = origin.split(",") # Removing commas 
-	no_comma_dest = destination.split(",") 
-
-	orig = list()
-	dest = list()
-	for o in no_comma_orig: 
-		orig.extend(o.split())
-	for d in no_comma_dest:
-		dest.extend(d.split())
-
-	params = {"origin": "+".join(orig),
-			  "destination": "+".join(dest),
-			  "key": API_KEY}
-
-	try: 
-		r = requests.get("https://maps.googleapis.com/maps/api/directions/json", params = params, timeout = 30)
-		return r.json()
-	except: 
-		print("Couldn't establish connection with the Google Maps API") # Probably change this later 
 	
+	if waypoint != None: 
+		try: 
+			return client.directions(origin, destination)
+		except: 
+			print("Couldn't establish connection with the Google Maps API") # Probably change this later 
+	else:
+		try: 
+			return client.directions(origin, destination, mode = "driving", waypoints = waypoint)
+		except:
+			print("Couldn't establish connection with the Google Maps API")
 
-def get_total_route_distance(jsn): 
+def get_total_route_distance(route): 
 	""" Returns the total route distance between in a route encoded in a json
 	
 	Assumes the JSON file follows Google Maps' route format 
 	"""
-	total_distance = jsn["routes"][0]["legs"][0]["distance"]
-	total_duration = jsn["routes"][0]["legs"][0]["duration"]
+	total_distance = route[0]["legs"][0]["distance"]
+	total_duration = route[0]["legs"][0]["duration"]
 	return total_distance, total_duration
 
-def get_lat_long_rect(jsn): 
+def get_lat_long_rect(route): 
 	""" Returns the latitudes and longitudes that bound the area we will be driving """
-	latitude_ne = jsn["routes"][0]["bounds"]["northeast"]["lat"]
-	longitude_ne = jsn["routes"][0]["bounds"]["northeast"]["lng"]
-	latitude_sw = jsn["routes"][0]["bounds"]["southwest"]["lat"]
-	longitude_sw = jsn["routes"][0]["bounds"]["southwest"]["lng"]
+	latitude_ne = route[0]["bounds"]["northeast"]["lat"]
+	longitude_ne = route[0]["bounds"]["northeast"]["lng"]
+	latitude_sw = route[0]["bounds"]["southwest"]["lat"]
+	longitude_sw = route[0]["bounds"]["southwest"]["lng"]
 	return (latitude_ne, longitude_ne), (latitude_sw, longitude_sw)
 
 def get_gas_stations(northeast, southwest): 
@@ -71,11 +62,15 @@ if __name__ == "__main__":
 	input_origin = input("Enter your starting point: ")
 	input_destination = input("Enter your destination: ")
 
-	jsn = generate_route(input_origin, input_destination)
-	northeast, southwest = get_lat_long_rect(jsn)
+	gmaps = googlemaps.Client(key = API_KEY)
+
+	route = generate_route(gmaps, input_origin, input_destination)
+	northeast, southwest = get_lat_long_rect(route)
 	gas_stations = get_gas_stations(northeast, southwest)
 	if len(gas_stations) == 0: # Couldn't find any prospective gas stations 
 		print("There are no gas stations between your start and end locations")
 		sys.exit(0)
 	else:
 		print(gas_stations)
+		# waypoint = gas_stations[0]
+		# print(generate_route(gmaps, input_origin, input_destination, waypoint))
